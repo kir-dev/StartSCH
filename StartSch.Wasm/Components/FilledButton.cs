c7585b74e3aea4e83ba324465e567fedc9ec4a0d
Albert Ragány-Németh <me@alb1.hu>
2024-12-22 22:21:21 +0100

    Add CommonButton and TextButton

---
+++ StartSch.Wasm/Components/FilledButton.cs
@@ -0,0 +1,6 @@
+namespace StartSch.Wasm.Components;
+
+public class FilledButton : CommonButton
+{
+    protected override string Type => "filled-button";
+}
---
+++ StartSch.Wasm/Components/TextButton.cs
@@ -0,0 +1,6 @@
+namespace StartSch.Wasm.Components;
+
+public class TextButton : CommonButton
+{
+    protected override string Type => "text-button";
+}
