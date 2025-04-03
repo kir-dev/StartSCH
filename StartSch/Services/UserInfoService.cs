e47be81609bf78b2664c301f5e59e1fab7c436bd
Albert Ragány-Németh <me@alb1.hu>
2025-04-03 14:24:47 +0200

    wip

--- StartSch/Services/UserInfoService.cs
+++ StartSch/Services/UserInfoService.cs
@@ -31,7 +31,7 @@ public class UserInfoService(Db db, IMemoryCache cache)
                 JsonSerializer.Serialize(
                     userInfo.PekActiveMemberships?
                         .Select(m => new GroupMembership(m.PekId, m.Name, m.Title))
-                        .Append(new(528, "Paschta", ["admin"]))
+//                        .Append(new(528, "Paschta", ["admin"]))
                         .ToList())));
         }
 
