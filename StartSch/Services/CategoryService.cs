8aa2c70ef55142c0ed96102012357efe00501937
Albert Ragány-Németh <me@alb1.hu>
2025-05-01 23:41:54 +0200

    Add Sqlite migration, fix deadlock on startup

--- StartSch/Services/CategoryService.cs
+++ StartSch/Services/CategoryService.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using StartSch.Data;
+using StartSch.Data.Migrations;
 
 namespace StartSch.Services;
 
@@ -12,16 +13,24 @@ public class CategoryService(IDbContextFactory<Db> dbFactory, Db scopeDb, IMemor
     {
         var cached = await cache.GetOrCreateAsync(CacheKey, async entry =>
         {
-            await using var db = await dbFactory.CreateDbContextAsync();
-            await using var tx = await db.BeginSnapshotTransactionAsync();
+            List<Page> pages;
             
-            var pages = await db.Pages
-                .Include(p => p.Categories)
-                .AsSplitQuery()
-                .AsNoTrackingWithIdentityResolution()
-                .ToListAsync();
-            
-            await tx.CommitAsync();
+            // SQLite doesn't really support parallel transactions. The current method might be called from inside
+            // another one, let's just reuse that transaction. `AsNoTrackingWithIdentityResolution` ensures the
+            // results from the query won't be leaked to the calling code through the DbContext.
+            if (scopeDb is SqliteDb && scopeDb.Database.CurrentTransaction != null)
+            {
+                pages = await GetPages(scopeDb);
+            }
+            else
+            {
+                await using var db = await dbFactory.CreateDbContextAsync();
+                await using var tx = await db.BeginSnapshotTransactionAsync();
+
+                pages = await GetPages(db);
+                
+                await tx.CommitAsync();
+            }
             
             entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5);
             
@@ -31,5 +40,14 @@ public class CategoryService(IDbContextFactory<Db> dbFactory, Db scopeDb, IMemor
         CategoryIndex index = cached!.DeepCopy();
         index.Attach(scopeDb);
         return index;
+    }
+    
+    private static async Task<List<Page>> GetPages(Db db)
+    {
+        return await db.Pages
+            .Include(p => p.Categories)
+            .AsSplitQuery()
+            .AsNoTrackingWithIdentityResolution()
+            .ToListAsync();
     }
 }
