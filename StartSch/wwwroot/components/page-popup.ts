3dec6308b0748615edeec79227d9038b60a6681b
Albert Ragány-Németh <me@alb1.hu>
2025-06-12 19:58:48 +0200

    Add page link to page popup

--- StartSch/wwwroot/components/page-popup.ts
+++ StartSch/wwwroot/components/page-popup.ts
@@ -23,6 +23,24 @@ export class PagePopup extends InterestContainer {
             border-radius: 8px;
             box-shadow: var(--md-sys-shadow-2);
         }
+        
+        header>a {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            gap: 16px;
+            text-decoration: none;
+            color: var(--md-sys-color-on-tertiary-container);
+            font-family: "Roboto Serif", serif;
+            font-weight: bold;
+            font-variation-settings: "wdth" 0;
+            margin-bottom: 8px;
+            
+            h2 {
+                display: inline flex;
+                margin: 0;
+            }
+        }
     `;
 
     private _cleanup?: () => void;
@@ -53,6 +71,14 @@ export class PagePopup extends InterestContainer {
 
         return html`
             <div class="surface">
+                <header>
+                    <a href="/pages/${page.id}">
+                        <h2>${page.name}</h2>
+                        <md-icon>
+                            open_in_new
+                        </md-icon>
+                    </a>
+                </header>
                 ${page.categories.map(category => html`
                     <interest-toggles category="${category.id}"/>
                 `)}
