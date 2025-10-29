fcce88897117f2138f94abc3ed1ce562533c29dc
Albert Ragány-Németh <me@alb1.hu>
2025-10-29 22:20:35 +0100

    Cache

--- StartSch/Controllers/IcsController.cs
+++ StartSch/Controllers/IcsController.cs
@@ -5,68 +5,78 @@ using Ical.Net.DataTypes;
 using Ical.Net.Serialization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Options;
 using StartSch.Data;
 
 namespace StartSch.Controllers;
 
 [ApiController]
-public class IcsController(Db db, IOptions<StartSchOptions> options) : ControllerBase
+public class IcsController(
+    Db db,
+    IOptions<StartSchOptions> options,
+    IMemoryCache cache)
+    : ControllerBase
 {
     [HttpGet("/temp/everything.ics")]
     public async Task<string> GetEverythingIcs()
     {
-        Calendar calendar = new()
+        return (await cache.GetOrCreateAsync("everything.ics", async entry =>
         {
-            Properties = { new CalendarProperty("X-WR-CALNAME", "StartSCH") }
-        };
-        calendar.Events.AddRange(
-            (await db.Events.ToListAsync())
-            .Where(e => e.Start.HasValue)
-            .Select(e =>
+            entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5);
+            
+            Calendar calendar = new()
             {
-                string? description = !string.IsNullOrWhiteSpace(e.DescriptionMarkdown)
-                    ? new TextContent(e.DescriptionMarkdown, null).HtmlContent
-                    : null;
-                if (description == "") description = null;
-
-                StringBuilder sb = new();
-
-                if (description != null)
-                    sb.Append("<p>");
-
-                sb.Append("<a href=\"");
-                sb.Append(options.Value.PublicUrl);
-                sb.Append("/events/");
-                sb.Append(e.Id);
-                sb.Append("?utm_medium=ics");
-                sb.Append("\">StartSCH</a>");
-                if (e.ExternalUrl != null && Uri.TryCreate(e.ExternalUrl, UriKind.Absolute, out Uri? uri))
+                Properties = { new CalendarProperty("X-WR-CALNAME", "StartSCH") }
+            };
+            calendar.Events.AddRange(
+                (await db.Events.ToListAsync())
+                .Where(e => e.Start.HasValue)
+                .Select(e =>
                 {
-                    sb.Append(" | <a href=\"");
-                    sb.Append(e.ExternalUrl);
-                    sb.Append("\">");
-                    sb.Append(uri.Host);
-                    sb.Append("</a>");
-                }
+                    string? description = !string.IsNullOrWhiteSpace(e.DescriptionMarkdown)
+                        ? new TextContent(e.DescriptionMarkdown, null).HtmlContent
+                        : null;
+                    if (description == "") description = null;
 
-                if (description != null)
-                {
-                    sb.Append("</p>");
-                    sb.Append("<hr>");
-                    sb.Append(description);
-                }
+                    StringBuilder sb = new();
 
-                return new CalendarEvent()
-                {
-                    Start = new CalDateTime(e.Start!.Value),
-                    End = e.End.HasValue ? new CalDateTime(e.End.Value) : null,
-                    Summary = e.Title,
-                    Description = sb.ToString(),
-                };
-            })
-        );
+                    if (description != null)
+                        sb.Append("<p>");
 
-        return new CalendarSerializer().SerializeToString(calendar)!;
+                    sb.Append("<a href=\"");
+                    sb.Append(options.Value.PublicUrl);
+                    sb.Append("/events/");
+                    sb.Append(e.Id);
+                    sb.Append("?utm_medium=ics");
+                    sb.Append("\">StartSCH</a>");
+                    if (e.ExternalUrl != null && Uri.TryCreate(e.ExternalUrl, UriKind.Absolute, out Uri? uri))
+                    {
+                        sb.Append(" | <a href=\"");
+                        sb.Append(e.ExternalUrl);
+                        sb.Append("\">");
+                        sb.Append(uri.Host);
+                        sb.Append("</a>");
+                    }
+
+                    if (description != null)
+                    {
+                        sb.Append("</p>");
+                        sb.Append("<hr>");
+                        sb.Append(description);
+                    }
+
+                    return new CalendarEvent()
+                    {
+                        Start = new CalDateTime(e.Start!.Value),
+                        End = e.End.HasValue ? new CalDateTime(e.End.Value) : null,
+                        Summary = e.Title,
+                        Description = sb.ToString(),
+                    };
+                })
+            );
+
+            return new CalendarSerializer().SerializeToString(calendar)!;
+        }))!;
     }
 }
