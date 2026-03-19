2fad694cc2db7530b3e830c780e2501b9792a17c
Albert Ragány-Németh <me@alb1.hu>
2026-03-19 18:29:19 +0100

    wip

--- StartSch/Controllers/PersonalCalendarsController.cs
+++ StartSch/Controllers/PersonalCalendarsController.cs
@@ -13,14 +13,58 @@ public class PersonalCalendarsController(
     Db db
 ) : ControllerBase
 {
-    [HttpPost("decrypt-encryption-key"), Authorize]
-    public ActionResult<PersonalCalendarEncryptionToken> DecryptEncryptionKey([FromBody] string ciphertext)
+    [HttpPut, Authorize]
+    public async Task<ActionResult<PersonalCalendarLive>> CreateOrUpdate(
+        PersonalCalendarLive request,
+        [FromQuery(Name = "key")] string? protectedEncryptionToken
+    )
     {
         int userId = User.GetId();
-        var encryptionKey = PersonalCalendarEncryptionToken.Unprotect(ciphertext, dataProtectionProvider);
-        if (userId != encryptionKey.UserId)
-            return Unauthorized();
-        return encryptionKey;
+        PersonalCalendar? personalCalendar;
+        if (request.Id != 0)
+        {
+            personalCalendar = await db.PersonalCalendars.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (personalCalendar == null)
+                return NotFound();
+            if (personalCalendar.UserId != userId)
+                return Unauthorized();
+        }
+        else
+        {
+            personalCalendar = request switch
+            {
+                PersonalStartSchCalendarLive => new PersonalStartSchCalendar(),
+                PersonalNeptunCalendarLive => new PersonalNeptunCalendar(),
+                PersonalMoodleCalendarLive => new PersonalMoodleCalendar(),
+                _ => throw new InvalidOperationException(),
+            };
+            personalCalendar.UserId = userId;
+            db.PersonalCalendars.Add(personalCalendar);
+        }
+
+        personalCalendar.Name = request.Name;
+
+        if (request is ExternalPersonalCalendarLive externalPersonalCalendarRequest)
+        {
+            ArgumentNullException.ThrowIfNull(protectedEncryptionToken);
+            PersonalCalendarEncryptionToken encryptionToken =
+                PersonalCalendarEncryptionToken.Unprotect(protectedEncryptionToken, dataProtectionProvider);
+            if (encryptionToken.UserId != userId)
+                return Unauthorized("Encryption token belongs to a different user");
+
+            ExternalPersonalCalendar externalPersonalCalendar = (ExternalPersonalCalendar)personalCalendar;
+            externalPersonalCalendar.SetUrl(externalPersonalCalendarRequest.Url, encryptionToken.AesKey);
+        }
+
+        await db.SaveChangesAsync();
+
+        if (request.Id == 0)
+        {
+            request.Id = personalCalendar.Id;
+            return Created($"/calendars/personal/{request.Id}", request);
+        }
+
+        return NoContent();
     }
 
     [HttpPost("reset-encryption-key"), Authorize]
@@ -37,53 +81,5 @@ public class PersonalCalendarsController(
         return new ResetEncryptionKeyResult(
             new PersonalCalendarEncryptionToken(aesKey, userId).Protect(dataProtectionProvider)
         );
-    }
-
-    [HttpPut, Authorize]
-    public async Task<ActionResult<PersonalCalendarLive>> CreateOrUpdate(
-        PersonalCalendarLive request,
-        [FromQuery(Name = "key")] string? protectedEncryptionToken
-    )
-    {
-        int userId = User.GetId();
-        PersonalCalendar? personalCalendar;
-        if (request.Id != 0)
-        {
-            personalCalendar = await db.PersonalCalendars.FirstOrDefaultAsync(x => x.Id == request.Id);
-            if (personalCalendar == null)
-                return NotFound();
-        }
-        else
-        {
-            personalCalendar = request switch
-            {
-                PersonalStartSchCalendarLive => new PersonalStartSchCalendar(),
-                PersonalNeptunCalendarLive => new PersonalNeptunCalendar(),
-                PersonalMoodleCalendarLive => new PersonalMoodleCalendar(),
-                _ => throw new InvalidOperationException(),
-            };
-        }
-
-        personalCalendar.UserId = userId;
-        personalCalendar.Name = request.Name;
-
-        if (request is ExternalPersonalCalendarLive externalPersonalCalendarRequest)
-        {
-            ArgumentNullException.ThrowIfNull(protectedEncryptionToken);
-            PersonalCalendarEncryptionToken encryptionToken =
-                PersonalCalendarEncryptionToken.Unprotect(protectedEncryptionToken, dataProtectionProvider);
-            if (encryptionToken.UserId != userId)
-                return Unauthorized("Encryption token belongs to a different user");
-
-            ExternalPersonalCalendar externalPersonalCalendar = (ExternalPersonalCalendar)personalCalendar;
-            externalPersonalCalendar.SetUrl(externalPersonalCalendarRequest.Url, encryptionToken.AesKey);
-        }
-
-        db.PersonalCalendars.Add(personalCalendar);
-        await db.SaveChangesAsync();
-
-        request.Id = personalCalendar.Id;
-
-        return Created($"/calendars/personal/{request.Id}", request);
     }
 }
